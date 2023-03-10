e86265f229220ff3d959cb2b8eac0e1a57d44b69
KostasKilda <kostas.kilda@gmail.com>
2023-03-10 23:39:23 +0000

    Added multithreading so the program does freeze when it's downloading

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -8,6 +8,8 @@ from PIL import ImageTk, Image
 import io
 import time
 import re
+import os
+import threading
 
 
 defaultImgUrl = 'https://mir-s3-cdn-cf.behance.net/projects/404/305eeb62042495.Y3JvcCwxMzg0LDEwODMsMjcwLDA.jpg'
@@ -23,11 +25,6 @@ youtubePlaylistImg = 'https://static.wikia.nocookie.net/youtube/images/8/8e/Yout
 
 # Playlist
 # url = 'https://www.youtube.com/playlist?list=PLeNVp42ZDPZpqNq06kF9qZQYadQ-Ji1nt'
-
-# Stream
-# url = 'https://www.youtube.com/watch?v=jfKfPfyJRdk'
-# https://www.youtube.com/watch?v=jfKfPfyJRdk
-
 
 root = Tk()
 
@@ -78,8 +75,6 @@ def viewSource(i):
                 changeImage(YouTube(playlist[0]).thumbnail_url)
             except:
                 changeImage(youtubePlaylistImg)
-            # for video in playlist:
-                # print(tryLink(0, video))
         else:
             changeImage(defaultImgUrl)
             changeVideoTitle('None')
@@ -94,9 +89,12 @@ def viewSource(i):
 
 
 # Method that acts as a trigger event for "Search" button
-def sendInfo():
+def sendInfo(option):
     # Checking if the link is valid
     if(len(url.get())>5 and (url.get().lower().find('yout')!=-1)):
+        if(option==1):
+            thread = threading.Thread(target=startDownload)
+            thread.start()
         viewSource(0)
     else:
         changeImage(defaultImgUrl)
@@ -115,18 +113,18 @@ def changeImage(imageUrl):
 
 def findTitleNewLine(title):
     matches = [m.start() for m in re.finditer(' ', title)]
-    if matches != [] and matches[0]<24:
+    if matches != [] and matches[0]<18:
         i = len(matches)-1
         while i!=0:
-            if(matches[i]<=24):
-                return matches[i]
+            if(matches[i]<=18):
+                return matches[i]+1
             i-=1
     else:
-        return 24
+        return 18
         
 # Method that will change video title
 def changeVideoTitle(videoTitle):
-    if(len(videoTitle)>28):
+    if(len(videoTitle)>18):
         breakPoint = findTitleNewLine(videoTitle)
         if(len(videoTitle)>=62):
             videoName.configure(text="Video name: %s\n %s..." % (videoTitle[:breakPoint],videoTitle[breakPoint:61]))
@@ -135,15 +133,70 @@ def changeVideoTitle(videoTitle):
     else:
         videoName.configure(text="Video name: %s" % videoTitle)
 
+
+def createFolder():
+    downloadPath = str(Path.home() / "Downloads")
+    folderName = 'Youtube downloads'
+    if not os.path.exists(os.path.join(downloadPath, folderName)):
+        os.mkdir(os.path.join(downloadPath, folderName))
+    return os.path.join(downloadPath, folderName)
+
+def startDownload():
+    downloadPath = createFolder()
+    if(url.get().find('playlist?')!=-1):
+            playlistUrl = Playlist(url.get())
+
+            for video_url in playlistUrl.video_urls:
+                # download video
+                try:
+                    yt = YouTube(video_url)
+                    stream = yt.streams.get_highest_resolution()
+                    stream.download(output_path=downloadPath)
+                except: 
+                    try:
+                        yt = YouTube(video_url)
+                        stream = yt.streams.get_highest_resolution()
+                        stream.download(output_path=downloadPath)
+                    except: 
+                        try:
+                            yt = YouTube(video_url)
+                            stream = yt.streams.get_highest_resolution()
+                            stream.download(output_path=downloadPath)
+                        except:        
+                            print('Not a valid playlist')
+    else:
+        try:
+            yt = YouTube(url.get())
+            stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            stream.download(output_path=downloadPath)
+        except:
+            print('Not a valid video')
+
+
+
+def downLoad():
+    sendInfo(1)
+
+def search():
+    sendInfo(0)
+
 # Setting the window size and open location to the middle of the screen
-root.geometry('450x300+%d+%d' % ( root.winfo_screenwidth()/2.6,root.winfo_screenheight()/3.6))
+root.geometry('380x380+%d+%d' % ( root.winfo_screenwidth()/2.6,root.winfo_screenheight()/3.6))
 root.grid()
 
+style = ttk.Style()
 
-ttk.Label(root, text="Enter a link to a youtube video, stream or playlist", font=('Arial', 14), padding=(20)).grid(column=0, row=0, columnspan=2)
+
+
+ttk.Label(root, text="Enter a link to a youtube video or playlist", font=('Arial', 14), padding=(20), background='white').grid(column=0, row=0, columnspan=2)
 url = Entry(root ,width='46')
-ttk.Button(root, text="Search", command=sendInfo).grid(column=1, row=1)
+ttk.Button(root, text="Search", command=search).grid(column=1, row=1)
 videoName = Label(root, text="Video name: None", font=('Arial', 10), anchor='w', justify='left')
+
+style.theme_create('downloadstyle', parent='alt', settings={"Download.TButton": {"configure": {"foreground": "#ffffff", "background": "#4CAF50", "font": ('Arial', 10),
+        "padding": (8, 5), "borderwidth": 0, "bordercolor": "#4CAF50", "borderradius": "50%"}, "map": {"background": [("active", "#4CAF50"), ("pressed", "#4CAF50")]}}})
+style.theme_use('downloadstyle')
+ttk.Button(root, text="Download", command=downLoad, style='Download.TButton').grid(column=0, row=4, columnspan=2, sticky='s')
 
 
 # Setting the label for later image use
@@ -157,7 +210,6 @@ videoName.grid(column=0, row=3,sticky='w', padx=(170,0), pady=(30,140), columnsp
 
 # load the default image
 changeImage(defaultImgUrl)
-
 
 root.pack_slaves()
 root.mainloop()
